9ecbe51747e0b6154b92c22a84140978bc0bd0ab
Mitchell R <support@bettercorp.co.za>
2022-05-23 02:09:17 +0200

    Adding higher timeout for adbud...

--- src/clients/adbud/adbud.ts
+++ src/clients/adbud/adbud.ts
@@ -15,46 +15,46 @@ export class adbud extends CPluginClient<MyPluginConfig> {
     return await this.emitEventAndReturn<IAdBudRequest<undefined>, ICustomerData>("get-customer", {
       auth: config,
       data: undefined
-    });
+    }, 10000);
   }
   async setCustomer(config: IAdBudRequestAuth, data: ICustomerUpdateData): Promise<ICustomerData> {
     return await this.emitEventAndReturn<IAdBudRequest<ICustomerUpdateData>, ICustomerData>("set-customer", {
       auth: config,
       data: data
-    });
+    }, 15000);
   }
 
   async getSuggestions(config: IAdBudRequestAuth): Promise<ICustomerSuggestionData> {
     return await this.emitEventAndReturn<IAdBudRequest<undefined>, ICustomerSuggestionData>("get-suggestions", {
       auth: config,
       data: undefined
-    });
+    }, 10000);
   }
 
   async getSearchTerms(config: IAdBudRequestAuth): Promise<ISearchTermDatum[]> {
     return await this.emitEventAndReturn<IAdBudRequest<undefined>, ISearchTermDatum[]>("get-search-terms", {
       auth: config,
       data: undefined
-    });
+    }, 10000);
   }
 
   async setSearchTerm(config: IAdBudRequestAuth, term: string, wanted: boolean): Promise<ISetTermData> {
     return await this.emitEventAndReturn<IAdBudRequest<{ term: string, wanted: boolean; }>, ISetTermData>("set-search-term", {
       auth: config,
       data: { term, wanted }
-    });
+    }, 15000);
   }
 
   async getStats(config: IAdBudRequestAuth, query: IAdBudRequestStats): Promise<IDashboardData> {
     return await this.emitEventAndReturn<IAdBudRequest<IAdBudRequestStats>, IDashboardData>("get-stats", {
       auth: config,
       data: query
-    });
+    }, 10000);
   }
   async geoTarget(config: IAdBudRequestAuth, term: string, country: string): Promise<Array<IGeoTarget>> {
     return await this.emitEventAndReturn<IAdBudRequest<IGeoTargetRequest>, Array<IGeoTarget>>("get-geo-target", {
       auth: config,
       data: { term, country }
-    });
+    }, 10000);
   }
 }
