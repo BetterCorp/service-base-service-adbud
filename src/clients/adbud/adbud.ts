9275681a8111debfba3f42de5f65b6e9e047b5c7
Mitchell R <mitchell.robert@bettercorp.co.za>
2022-09-14 19:41:42 +0200

    Upgraded to BSB v8

--- src/clients/adbud/adbud.ts
+++ src/clients/adbud/adbud.ts
@@ -1,60 +1,322 @@
-import { CPluginClient } from "@bettercorp/service-base/lib/interfaces/plugins";
-import { ICustomerSuggestionData } from '../../plugins/adbud/ICustomerSuggestion';
-import { IAdBudRequest, IAdBudRequestAuth, IAdBudRequestStats } from '../../plugins/adbud/IAdBudRequest';
-import { ICustomerData, ICustomerUpdateData } from '../../plugins/adbud/ICustomer';
-import { MyPluginConfig } from '../../plugins/adbud/sec.config';
-import { ISearchTermDatum } from '../../plugins/adbud/ISearchTerm';
-import { IDashboardData } from '../../plugins/adbud/IDashboard';
-import { ISetTermData } from '../../plugins/adbud/ISetTerm';
-import { IGeoTarget, IGeoTargetRequest } from '../../plugins/adbud/IGeoTarget';
+import { ICustomerSuggestionData } from "../../plugins/adbud/ICustomerSuggestion";
+import { IAdBudRequestAuth } from "../../plugins/adbud/IAdBudRequest";
+import {
+  ICustomerData,
+  ICustomerUpdateData,
+} from "../../plugins/adbud/ICustomer";
+import { MyPluginConfig } from "../../plugins/adbud/sec.config";
+import { ISearchTermDatum } from "../../plugins/adbud/ISearchTerm";
+import {
+  IDashboardData,
+  IDashboardSelector,
+} from "../../plugins/adbud/IDashboard";
+import { ISetTermData } from "../../plugins/adbud/ISetTerm";
+import { IGeoTarget } from "../../plugins/adbud/IGeoTarget";
+import {
+  ServiceCallable,
+  ServicesBase,
+  ServicesClient,
+} from "@bettercorp/service-base";
+import { ADBudReturnable } from "../../plugins/adbud/plugin";
 
-export class adbud extends CPluginClient<MyPluginConfig> {
-  public readonly _pluginName: string = "adbud";
+const TIMEOUT_GET = 10000;
+const TIMEOUT_SET = 15000;
+export class ADBudClient extends ServicesClient<
+  ServiceCallable,
+  ServiceCallable,
+  ADBudReturnable,
+  ServiceCallable,
+  ServiceCallable,
+  MyPluginConfig
+> {
+  public readonly _pluginName: string = "service-adbud";
+  constructor(self: ServicesBase) {
+    super(self);
+  }
+  async getCustomer(
+    host: string,
+    username: string,
+    password: string
+  ): Promise<ICustomerData>;
+  async getCustomer(
+    host: string,
+    username: string,
+    password: string,
+    customerId?: string | undefined
+  ): Promise<ICustomerData>;
+  async getCustomer(auth: IAdBudRequestAuth): Promise<ICustomerData>;
+  async getCustomer(
+    hostOrAuth: string | IAdBudRequestAuth,
+    username?: string,
+    password?: string,
+    customerId?: string | undefined
+  ): Promise<ICustomerData> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username,
+            password,
+            customerId,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "getCustomer",
+      TIMEOUT_GET,
+      authObject.host,
+      authObject.username!,
+      authObject.password!,
+      authObject.customerId
+    );
+  }
 
-  async getCustomer(config: IAdBudRequestAuth): Promise<ICustomerData> {
-    return await this.emitEventAndReturn<IAdBudRequest<undefined>, ICustomerData>("get-customer", {
-      auth: config,
-      data: undefined
-    }, 10000);
-  }
-  async setCustomer(config: IAdBudRequestAuth, data: ICustomerUpdateData): Promise<ICustomerData> {
-    return await this.emitEventAndReturn<IAdBudRequest<ICustomerUpdateData>, ICustomerData>("set-customer", {
-      auth: config,
-      data: data
-    }, 15000);
+  async setCustomer(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    customer: ICustomerUpdateData
+  ): Promise<ICustomerData>;
+  async setCustomer(
+    auth: IAdBudRequestAuth,
+    customer: ICustomerUpdateData
+  ): Promise<ICustomerData>;
+  async setCustomer(
+    hostOrAuth: string | IAdBudRequestAuth,
+    username: string | ICustomerUpdateData,
+    password?: string,
+    customerId?: string,
+    customer?: ICustomerUpdateData
+  ): Promise<ICustomerData> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username,
+            password,
+            customerId,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "setCustomer",
+      TIMEOUT_SET,
+      authObject.host,
+      authObject.username as string,
+      authObject.password as string,
+      authObject.customerId as string,
+      typeof hostOrAuth === "string"
+        ? (username as ICustomerUpdateData)
+        : customer!
+    );
   }
 
-  async getSuggestions(config: IAdBudRequestAuth): Promise<ICustomerSuggestionData> {
-    return await this.emitEventAndReturn<IAdBudRequest<undefined>, ICustomerSuggestionData>("get-suggestions", {
-      auth: config,
-      data: undefined
-    }, 10000);
+  async getSuggestions(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string
+  ): Promise<ICustomerSuggestionData>;
+  async getSuggestions(
+    auth: IAdBudRequestAuth
+  ): Promise<ICustomerSuggestionData>;
+  async getSuggestions(
+    hostOrAuth: string | IAdBudRequestAuth,
+    username?: string,
+    password?: string,
+    customerId?: string
+  ): Promise<ICustomerSuggestionData> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username,
+            password,
+            customerId,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "getSuggestions",
+      TIMEOUT_GET,
+      authObject.host,
+      authObject.username as string,
+      authObject.password as string,
+      authObject.customerId as string
+    );
   }
 
-  async getSearchTerms(config: IAdBudRequestAuth): Promise<ISearchTermDatum[]> {
-    return await this.emitEventAndReturn<IAdBudRequest<undefined>, ISearchTermDatum[]>("get-search-terms", {
-      auth: config,
-      data: undefined
-    }, 10000);
+  async getSearchTerms(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string
+  ): Promise<ISearchTermDatum[]>;
+  async getSearchTerms(auth: IAdBudRequestAuth): Promise<ISearchTermDatum[]>;
+  async getSearchTerms(
+    hostOrAuth: string | IAdBudRequestAuth,
+    username?: string,
+    password?: string,
+    customerId?: string
+  ): Promise<ISearchTermDatum[]> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username,
+            password,
+            customerId,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "getSearchTerms",
+      TIMEOUT_GET,
+      authObject.host,
+      authObject.username as string,
+      authObject.password as string,
+      authObject.customerId as string
+    );
   }
 
-  async setSearchTerm(config: IAdBudRequestAuth, term: string, wanted: boolean): Promise<ISetTermData> {
-    return await this.emitEventAndReturn<IAdBudRequest<{ term: string, wanted: boolean; }>, ISetTermData>("set-search-term", {
-      auth: config,
-      data: { term, wanted }
-    }, 15000);
+  async setSearchTerms(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    term: string,
+    wanted: boolean
+  ): Promise<ISetTermData>;
+  async setSearchTerms(
+    auth: IAdBudRequestAuth,
+    term: string,
+    wanted: boolean
+  ): Promise<ISetTermData>;
+  async setSearchTerms(
+    hostOrAuth: string | IAdBudRequestAuth,
+    usernameOrTerm: string,
+    passwordOrWanted: string | boolean,
+    customerId?: string,
+    term?: string,
+    wanted?: boolean
+  ): Promise<ISetTermData> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username: usernameOrTerm,
+            password: passwordOrWanted,
+            customerId,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "setSearchTerms",
+      TIMEOUT_SET,
+      authObject.host,
+      authObject.username,
+      authObject.password as string,
+      authObject.customerId as string,
+      typeof hostOrAuth === "string"
+        ? (term as string)
+        : (usernameOrTerm as string),
+      typeof hostOrAuth === "string"
+        ? (wanted as boolean)
+        : (passwordOrWanted as boolean)
+    );
   }
 
-  async getStats(config: IAdBudRequestAuth, query: IAdBudRequestStats): Promise<IDashboardData> {
-    return await this.emitEventAndReturn<IAdBudRequest<IAdBudRequestStats>, IDashboardData>("get-stats", {
-      auth: config,
-      data: query
-    }, 10000);
+  async getStats(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    startDate: number,
+    endDate: number,
+    selectors: IDashboardSelector[],
+    hideCosts: boolean
+  ): Promise<IDashboardData>;
+  async getStats(
+    auth: IAdBudRequestAuth,
+    startDate: number,
+    endDate: number,
+    selectors: IDashboardSelector[],
+    hideCosts: boolean
+  ): Promise<IDashboardData>;
+  async getStats(
+    hostOrAuth: string | IAdBudRequestAuth,
+    usernameOrStartDate: string | number,
+    passwordOrEndDate: string | number,
+    customerIdOrSelectors: string | IDashboardSelector[],
+    startDateOrHideCosts: number | boolean,
+    endDate?: number,
+    selectors?: IDashboardSelector[],
+    hideCosts?: boolean
+  ): Promise<IDashboardData> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username: usernameOrStartDate as string,
+            password: passwordOrEndDate as string,
+            customerId: customerIdOrSelectors as string,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "getStats",
+      TIMEOUT_GET,
+      authObject.host,
+      authObject.username,
+      authObject.password,
+      authObject.customerId as string,
+      (typeof hostOrAuth === "string"
+        ? startDateOrHideCosts
+        : usernameOrStartDate) as number,
+      (typeof hostOrAuth === "string" ? endDate : passwordOrEndDate) as number,
+      (typeof hostOrAuth === "string"
+        ? selectors
+        : customerIdOrSelectors) as IDashboardSelector[],
+      (typeof hostOrAuth === "string"
+        ? hideCosts
+        : startDateOrHideCosts) as boolean
+    );
   }
-  async geoTarget(config: IAdBudRequestAuth, term: string, country: string): Promise<Array<IGeoTarget>> {
-    return await this.emitEventAndReturn<IAdBudRequest<IGeoTargetRequest>, Array<IGeoTarget>>("get-geo-target", {
-      auth: config,
-      data: { term, country }
-    }, 10000);
+
+  async getGeoTargets(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    term: string,
+    country: string
+  ): Promise<IGeoTarget[]>;
+  async getGeoTargets(
+    auth: IAdBudRequestAuth,
+    term: string,
+    country: string
+  ): Promise<IGeoTarget[]>;
+  async getGeoTargets(
+    hostOrAuth: string | IAdBudRequestAuth,
+    usernameOrTerm: string,
+    passwordOrCountry: string,
+    customerId?: string,
+    term?: string,
+    country?: string
+  ): Promise<IGeoTarget[]> {
+    let authObject =
+      typeof hostOrAuth === "string"
+        ? {
+            host: hostOrAuth,
+            username: usernameOrTerm as string,
+            password: passwordOrCountry as string,
+            customerId: customerId as string,
+          }
+        : hostOrAuth;
+    return await this._plugin.emitEventAndReturnTimed(
+      "getGeoTargets",
+      TIMEOUT_GET,
+      authObject.host,
+      authObject.username,
+      authObject.password,
+      authObject.customerId as string,
+      (hostOrAuth === "string" ? term : usernameOrTerm) as string,
+      (hostOrAuth === "string" ? country : passwordOrCountry) as string
+    );
   }
 }
--- src/plugins/adbud/plugin.ts
+++ src/plugins/adbud/plugin.ts
@@ -1,129 +1,180 @@
-import { CPlugin } from "@bettercorp/service-base/lib/interfaces/plugins";
 import { MyPluginConfig } from "./sec.config";
-import { Tools } from "@bettercorp/tools/lib/Tools";
 import { adbud } from "./adbud";
-import { IAdBudRequest, IAdBudRequestStats } from "./IAdBudRequest";
 import { ICustomerData, ICustomerUpdateData } from "./ICustomer";
 import { ICustomerSuggestionData } from "./ICustomerSuggestion";
 import { ISearchTermDatum } from "./ISearchTerm";
-import { IDashboardData } from "./IDashboard";
+import { IDashboardData, IDashboardSelector } from "./IDashboard";
 import { ISetTermData } from "./ISetTerm";
-import { IGeoTarget, IGeoTargetRequest } from "./IGeoTarget";
+import { IGeoTarget } from "./IGeoTarget";
+import { ServiceCallable, ServicesBase } from "@bettercorp/service-base";
 
-export class Plugin extends CPlugin<MyPluginConfig> {
-  async init(): Promise<void> {
-    await this.onReturnableEvent<IAdBudRequest<undefined>, ICustomerData>(
-      null,
-      "get-customer",
-      async (data?) => {
-        if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-        const adbudClient = new adbud(
-          data!.auth.host,
-          data!.auth.username,
-          data!.auth.password,
-          data!.auth.customerId
-        );
+export interface ADBudReturnable extends ServiceCallable {
+  getCustomer(
+    host: string,
+    username: string,
+    password: string,
+    customerId?: string
+  ): Promise<ICustomerData>;
+  setCustomer(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    customer: ICustomerUpdateData
+  ): Promise<ICustomerData>;
+  getSuggestions(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string
+  ): Promise<ICustomerSuggestionData>;
+  getSearchTerms(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string
+  ): Promise<Array<ISearchTermDatum>>;
+  setSearchTerms(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    term: string,
+    wanted: boolean
+  ): Promise<ISetTermData>;
+  getStats(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    startDate: number,
+    endDate: number,
+    selectors: Array<IDashboardSelector>,
+    hideCosts: boolean
+  ): Promise<IDashboardData>;
+  getGeoTargets(
+    host: string,
+    username: string,
+    password: string,
+    customerId: string,
+    term: string,
+    country: string
+  ): Promise<Array<IGeoTarget>>;
+}
+
+export class Service extends ServicesBase<
+  ServiceCallable,
+  ServiceCallable,
+  ADBudReturnable,
+  ServiceCallable,
+  ServiceCallable,
+  MyPluginConfig
+> {
+  async init() {
+    this.onReturnableEvent(
+      "getCustomer",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId?: string
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
         await adbudClient.login();
         return await adbudClient.getCustomer();
       }
     );
-    await this.onReturnableEvent<
-      IAdBudRequest<ICustomerUpdateData>,
-      ICustomerData
-    >(null, "set-customer", async (data?) => {
-      if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-      const adbudClient = new adbud(
-        data!.auth.host,
-        data!.auth.username,
-        data!.auth.password,
-        data!.auth.customerId
-      );
-      await adbudClient.login();
-      return await adbudClient.setCustomer(data!.data);
-    });
-
-    await this.onReturnableEvent<
-      IAdBudRequest<undefined>,
-      ICustomerSuggestionData
-    >(null, "get-suggestions", async (data?) => {
-      if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-      const adbudClient = new adbud(
-        data!.auth.host,
-        data!.auth.username,
-        data!.auth.password,
-        data!.auth.customerId
-      );
-      await adbudClient.login();
-      return await adbudClient.getSuggestions();
-    });
-
-    await this.onReturnableEvent<IAdBudRequest<undefined>, ISearchTermDatum[]>(
-      null,
-      "get-search-terms",
-      async (data?) => {
-        if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-        const adbudClient = new adbud(
-          data!.auth.host,
-          data!.auth.username,
-          data!.auth.password,
-          data!.auth.customerId
-        );
+    this.onReturnableEvent(
+      "setCustomer",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string,
+        customer: ICustomerUpdateData
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
+        await adbudClient.login();
+        return await adbudClient.setCustomer(customer);
+      }
+    );
+    this.onReturnableEvent(
+      "getSuggestions",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
+        await adbudClient.login();
+        return await adbudClient.getSuggestions();
+      }
+    );
+    this.onReturnableEvent(
+      "getSearchTerms",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
         await adbudClient.login();
         return await adbudClient.getSearchTerms();
       }
     );
-
-    await this.onReturnableEvent<
-      IAdBudRequest<{ term: string; wanted: boolean }>,
-      ISetTermData
-    >(null, "set-search-term", async (data?) => {
-      if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-      const adbudClient = new adbud(
-        data!.auth.host,
-        data!.auth.username,
-        data!.auth.password,
-        data!.auth.customerId
-      );
-      await adbudClient.login();
-      return await adbudClient.setSearchTerm(
-        data!.data.term,
-        data!.data.wanted
-      );
-    });
-
-    await this.onReturnableEvent<
-      IAdBudRequest<IAdBudRequestStats>,
-      IDashboardData
-    >(null, "get-stats", async (data?) => {
-      if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-      const adbudClient = new adbud(
-        data!.auth.host,
-        data!.auth.username,
-        data!.auth.password,
-        data!.auth.customerId
-      );
-      await adbudClient.login();
-      return await adbudClient.getStats(
-        data!.data.startDate,
-        data!.data.endDate,
-        data!.data.selectors,
-        data!.data.hideCosts
-      );
-    });
-
-    await this.onReturnableEvent<
-      IAdBudRequest<IGeoTargetRequest>,
-      Array<IGeoTarget>
-    >(null, "get-geo-target", async (data?) => {
-      if (Tools.isNullOrUndefined(data)) throw "Undefined Data!";
-      const adbudClient = new adbud(
-        data!.auth.host,
-        data!.auth.username,
-        data!.auth.password,
-        data!.auth.customerId
-      );
-      await adbudClient.login();
-      return await adbudClient.geoTarget(data!.data.term, data!.data.country);
-    });
+    this.onReturnableEvent(
+      "setSearchTerms",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string,
+        term: string,
+        wanted: boolean
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
+        await adbudClient.login();
+        return await adbudClient.setSearchTerm(term, wanted);
+      }
+    );
+    this.onReturnableEvent(
+      "getStats",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string,
+        startDate: number,
+        endDate: number,
+        selectors: Array<IDashboardSelector>,
+        hideCosts: boolean
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
+        await adbudClient.login();
+        return await adbudClient.getStats(
+          startDate,
+          endDate,
+          selectors,
+          hideCosts
+        );
+      }
+    );
+    this.onReturnableEvent(
+      "getGeoTargets",
+      async (
+        host: string,
+        username: string,
+        password: string,
+        customerId: string,
+        term: string,
+        country: string
+      ) => {
+        const adbudClient = new adbud(host, username, password, customerId);
+        await adbudClient.login();
+        return await adbudClient.geoTarget(term, country);
+      }
+    );
   }
 }
