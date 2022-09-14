34e9987fb38ca8734d7ef4d223cb6700a175c8f3
Mitchell R <mitchell.robert@bettercorp.co.za>
2022-09-14 20:54:12 +0200

    Updated service ref for bsb v8 plugins. Updated sec config

---
+++ src/plugins/service-adbud/sec.config.ts
@@ -0,0 +1,14 @@
+import { SecConfig } from '@bettercorp/service-base';
+
+export interface MyPluginConfig {
+
+}
+
+export class Config extends SecConfig<MyPluginConfig> {
+  migrate(
+    mappedPluginName: string,
+    existingConfig: MyPluginConfig
+  ): MyPluginConfig {
+    return {};
+  }
+}
